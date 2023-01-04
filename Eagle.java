c77b4715c5c481c7fe1cbc0752596654b7b15be4
Bertrand Briset <82585037+BBriset@users.noreply.github.com>
2023-01-04 11:14:56 +0100

    Fly like an eagle

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -13,6 +13,10 @@ public class Eagle extends Bird {
         return altitude;
     }
 
+    public void setAltitude(int altitude){
+        this.altitude=altitude;
+    }
+
     public boolean isFlying() {
         return flying;
     }
@@ -20,5 +24,33 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+    @Override
+    public void takeOff() {
+        System.out.println(this.getName() +" takes off in the sky.");
+    }
+    @Override
+    public void land() {
+        if(getAltitude()<=1){
+            System.out.println(this.getName() +" lands on the ground");
+        }
+        else{
+            System.out.println(this.getName() +" is too high, it can't lands.");
+        }
+    }
+    @Override
+    public void ascend(int i) {
+        setAltitude(getAltitude()+i);
+        System.out.println(this.getName() +" flies upward, altitude : "+getAltitude());
+        
+    }
+    @Override
+    public void glide() {
+        System.out.println(" It glides into the air.");
+    }
+    @Override
+    public void descend(int i) {
+        setAltitude(getAltitude()-i);
+        System.out.println(this.getName() +" flies downward, altitude : "+getAltitude());
     }
 }
---
+++ Fly.java
@@ -0,0 +1,12 @@
+public interface Fly {
+
+  void takeOff();
+
+  void ascend(int i) ;
+  
+  void glide() ;
+  
+  void descend(int i) ;
+  
+  void land() ;
+}
